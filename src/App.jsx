34fb4bd38eb878c90c6096e105d35c99008043e0
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-10-12 17:02:25 +0530

    init loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,46 @@ import { apiUrl, filterData } from "./data";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true)
 
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      // console.log(data)
+      setCourses(data.data);
+    } catch (error) {
+      toast.error("something went wrong");
+    }
+    setLoading(false);
+  };
+  
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        console.log(data)
-        setCourses(data.data);
-      } catch (error) {
-        toast.error("something went wrong");
-      }
-    };
     fetchData();
   }, []);
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
 
-      <Filter filterData={filterData} />
+      <div>
+        <Filter filterData={filterData} />
+      </div>
 
-      {courses && <Cards courses={courses} />}
+      <div>
+        {
+          loading? (<Spinner/>) : (<Cards courses={courses} />)
+        }
+        
+      </div>
     </div>
   );
 }
